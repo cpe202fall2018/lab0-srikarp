addf774a3d9ef2964bf9bb6220f596b7dceaa7bf
srikar <srikarp18@gmail.com>
2018-09-24 17:19:33 -0700

    remove extra spaces

--- planets.py
+++ planets.py
@@ -3,7 +3,7 @@ def weight_on_planets():
 
   earthWeightInput = input("What do you weigh on earth? ")
   earthWeightFloat = float(earthWeightInput)
-  print ("\nOn Mars you would weigh ", earthWeightFloat*0.38, " pounds.\n" + "On Jupiter you would weigh ", earthWeightFloat*2.34, " pounds.") 
+  print ("\nOn Mars you would weigh", earthWeightFloat*0.38, "pounds.\n" + "On Jupiter you would weigh", earthWeightFloat*2.34, "pounds.") 
 
    
 if __name__ == '__main__':
